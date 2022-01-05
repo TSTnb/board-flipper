3500e45c5e3cbb0835df637a4111c50ef52eff1f
TSTman <tstman@protonmail.com>
2022-01-05 12:26:35 -0700

    New userscript

---
+++ board-flipper.user.js
@@ -0,0 +1,28 @@
+/* Copyright 2022 TSTman
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+// ==UserScript==
+// @name         HD Wiki Board Flipper
+// @namespace    HD Wiki Board Flipper
+// @license      Apache 2.0
+// @version      v0.0.0
+// @description  Buttons to flip the stacking boards on the Hard Drop wiki
+// @homepage     https://github.com/TSTman/board-flipper
+// @author       TSTman
+// @match        https://harddrop.com/wiki/*
+// @grant        none
+// ==/UserScript==
+(() => {
+    'use strict';
+})();
