d5ef326a5440762781d36bd05828fd6e707c9d14
TSTman <tstman@protonmail.com>
2022-01-05 17:28:19 -0700

    Change mino colors when flipping boards

--- board-flipper.user.js
+++ board-flipper.user.js
@@ -50,6 +50,19 @@
         );
     }
 
+    const minoProperties = {
+        "Tet.png": {"reverse": "Tet.png", "attributes": {"a": {"title": "", "href": "/wiki/File:Tet.png"}, "img": {"alt": "Tet.png", "src": "/w/images/1/18/Tet.png"}}},
+        "J": {"reverse": "L", "attributes": {"a": {"title": "J", "href": "/wiki/File:JTet.png"}, "img": {"alt": "J", "src": "/w/images/8/85/JTet.png"}}},
+        "i": {"reverse": "i", "attributes": {"a": {"title": "i", "href": "/wiki/File:ITet.png"}, "img": {"alt": "i", "src": "/w/images/1/19/ITet.png"}}},
+        "Z": {"reverse": "S", "attributes": {"a": {"title": "Z", "href": "/wiki/File:ZTet.png"}, "img": {"alt": "Z", "src": "/w/images/c/c6/ZTet.png"}}},
+        "S": {"reverse": "Z", "attributes": {"a": {"title": "S", "href": "/wiki/File:STet.png"}, "img": {"alt": "S", "src": "/w/images/b/bc/STet.png"}}},
+        "L": {"reverse": "J", "attributes": {"a": {"title": "L", "href": "/wiki/File:LTet.png"}, "img": {"alt": "L", "src": "/w/images/5/51/LTet.png"}}},
+        "O": {"reverse": "O", "attributes": {"a": {"title": "O", "href": "/wiki/File:OTet.png"}, "img": {"alt": "O", "src": "/w/images/2/20/OTet.png"}}},
+        "T": {"reverse": "T", "attributes": {"a": {"title": "T", "href": "/wiki/File:TTet.png"}, "img": {"alt": "T", "src": "/w/images/2/20/TTet.png"}}},
+        "G": {"reverse": "G", "attributes": {"a": {"title": "G", "href": "/wiki/File:GTet.png"}, "img": {"alt": "G", "src": "/w/images/8/88/GTet.png"}}},
+        "I": {"reverse": "I", "attributes": {"a": {"title": "I", "href": "/wiki/File:ITet.png"}, "img": {"alt": "I", "src": "/w/images/1/19/ITet.png"}}},
+    };
+
     function flipBoard(button) {
         let scope;
         if (button instanceof HTMLElement) {
@@ -60,6 +73,17 @@
         const pattern = `:scope div > ${Array(8).fill('a.image[href^="/wiki/File:"][href$="Tet.png"]').join(' + ')}`;
         [...scope.querySelectorAll(pattern)].map(el => el.parentElement).forEach(row => {
             const children = [...row.childNodes].reverse();
+            children.forEach(anchor => {
+                const elements = {
+                    a: anchor,
+                    img: anchor.querySelector(':scope > img'),
+                };
+                const properties = minoProperties[minoProperties[elements.img.alt].reverse];
+                Object.keys(elements).forEach(tag =>
+                    Object.keys(properties.attributes[tag]).forEach(attribute =>
+                        elements[tag].setAttribute(attribute, properties.attributes[tag][attribute])
+                    ))
+            });
             children.forEach(child => row.appendChild(child));
         });
     }
